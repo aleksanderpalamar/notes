fe121d417f318d62b1523fc15ceb37cac2b306a7
aleksanderpalamar <apalamar@live.com>
2023-10-24 11:32:21 -0300

    ADD: creating a function handleExpand and handleOnCreate

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ItemProps = {
   id?: Id<"documents">;
@@ -30,6 +34,32 @@ export const Item = ({
   level = 0,
   onExpand,
 }: ItemProps) => {
+  const router = useRouter()
+  const create = useMutation(api.documents.create)
+
+  const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+     e.stopPropagation();
+     onExpand?.();   
+  }
+
+  const handleOnCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    if (!id) return
+    const promise = create({ title: "Untitled", parentDocument: id })
+      .then((documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        //router.push(`/documents/${documentId}`)
+      })
+      
+      toast.promise(promise, {
+        loading: "Creating a new note...",
+        success: "New note created!",
+        error: "Failed to create a new note",
+      })
+  }
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
@@ -48,8 +78,8 @@ export const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-neutral-300
-        dark:bg-neutral-600 mr-1"
-          onClick={() => {}}
+        dark:bg-neutral-600 dark:hover:bg-neutral-500 mr-1"
+          onClick={handleExpand}
         >
           <ChevronIcon 
             className="h-4 w-4 shrink-0 text-muted-foreground/50"
@@ -73,6 +103,17 @@ export const Item = ({
         opacity-100">
           <span className="text-xs">⌘</span>k
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div 
+          role="button"
+          onClick={handleOnCreate}
+          className="opacity-0 group-hover:opacity-100 h-full ml-auto
+          rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+            <Plus className="h-4 w-4 text-muted-foreground"/>
+          </div>
+        </div>
       )}           
     </div>
   );
