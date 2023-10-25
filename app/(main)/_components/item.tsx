40bd129065765948bd4533ee64a25ba0ff516317
aleksanderpalamar <apalamar@live.com>
2023-10-25 11:00:18 -0300

    ADD: creating archive functionality and soft delete document

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -5,9 +5,17 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu"
+import { useUser } from "@clerk/clerk-react";
 
 type ItemProps = {
   id?: Id<"documents">;
@@ -34,8 +42,22 @@ export const Item = ({
   level = 0,
   onExpand,
 }: ItemProps) => {
+  const { user } = useUser()
   const router = useRouter()
   const create = useMutation(api.documents.create)
+  const archive = useMutation(api.documents.archives)
+
+  const handleOnArchive = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    if (!id) return
+    const promise = archive({ id })
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Document moved to trash!",
+      error: "Failed to archive document",
+    })
+  }
 
   const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
      e.stopPropagation();
@@ -106,6 +128,24 @@ export const Item = ({
       )}
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div role="button" className="opacity-0 group-hover:opacity-100 h-full ml-auto
+              rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                <MoreHorizontal className="h-4 w-4 text-muted-foreground"/>
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-60" align="start" side="right" forceMount>
+              <DropdownMenuItem onClick={handleOnArchive}>
+                <Trash className="h-4 w-4 text-muted-foreground"/>
+                <span className="ml-2">Delete</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="text-xs text-muted-foreground p-2 font-medium">
+                Last edited by: {user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div 
           role="button"
           onClick={handleOnCreate}
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,55 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+export const archives = mutation({
+  args: { id: v.id("documents")},
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    
+    if (!identity) {
+      throw new Error("Not logged in");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+    
+    if (existingDocument.userId !== userId) {
+      throw new Error("Not authorized");
+    }
+
+    const recursiveArchives = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => (
+          q
+            .eq("userId", userId)
+            .eq("parentDocument", documentId)
+        ))
+        .collect()
+
+        for (const child of children) {
+          await ctx.db.patch(child._id, {
+            isArchived: true
+          })
+
+          await recursiveArchives(child._id)
+        }
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true      
+    })
+
+    recursiveArchives(args.id)
+
+    return document
+  }
+})
 
 export const getSidebar = query({
   args: {
